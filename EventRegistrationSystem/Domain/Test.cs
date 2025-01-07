fd1951345d4c270389f078f35857f96d793525a1
Johnathan Sim <johnathansylmicrosoft@gmail.com>
2025-01-07 11:45:32 +0800

    Registrations

---
+++ EventRegistrationSystem/Domain/Test.cs
@@ -0,0 +1,10 @@
+﻿namespace EventRegistrationSystem.Domain
+{
+    public class Registrations : BaseDomainModel
+    {
+        public DateTime RegistrationDate { get; set; }
+        public int SeatsReserved {get; set;}
+        public int UserID {get; set;}
+        public int EventID {get; set;} 
+    }
+}
