5fa430f2813cb533004f148007b16a1752d3d2f7
RIC LEE <chuperic@gmail.com>
2025-01-13 17:26:55 +0800

    User domain v1

---
+++ EventRegistrationSystem/Domain/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace EventRegistrationSystem.Domain
+{
+    public class Users : BaseDomainModel 
+    {
+        public string? Name {get; set; }
+        public string? Email {get; set; }
+        public string? Password {get; set; }
+        public int PhoneNumber {get; set; }
+        public string? Role {get; set; }
+        public string? Preferences {get; set; }
+    }
+}
